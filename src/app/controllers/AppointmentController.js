279be2b10eb34b2975ebc3362636938e6f142ad8
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-09-04 00:00:23 -0300

    Add date-fns e verificação de datas disponiveis e conflitos de agendamento

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,69 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      date: Yup.date().required(),
+      provider_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const { date, provider_id } = req.body;
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    // Verifica se o usuário informado é prestador de serviço
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers.' });
+    }
+
+    // Verifica se o usuário informado é próprio usuário
+    if (provider_id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'You can not create appointments with yourself.' });
+    }
+
+    // Converte o date para um formato Date() do Javascript e dá um 'truncate' na hora
+    // convertendo 19:30 para 19:00 por exemplo
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not allowed.' });
+    }
+
+    // Checa se o horário está disponível
+    const checkAvailable = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailable) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available.' });
+    }
+
+    const appointment = await Appointment.create({
+      provider_id,
+      date,
+      user_id: req.userId,
+    });
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
